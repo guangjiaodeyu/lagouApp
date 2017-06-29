a31e6b92809881a76569f17993d0394de998240f
guangjiaodeyu <724709547@qq.com>
2017-06-29 11:27:06 +0800

    favorite page logic set

--- src/script/controller/favoriteCtrl.js
+++ src/script/controller/favoriteCtrl.js
@@ -4,5 +4,7 @@
 'use strict';
 angular.module('app')
 .controller('favoriteCtrl',['$scope','$http',function ($scope,$http) {
-
+    $http.get('data/myFavorite.json').success(function (res) {
+        $scope.positionList = res;
+    })
 }]);
--- src/script/directive/positionList.js
+++ src/script/directive/positionList.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2017/6/20.
  */
 'use strict';
-angular.module('app').directive('appPositionList',[function () {
+angular.module('app').directive('appPositionList',['cache','$http',function (cache,$http) {
     return {
         restrict:'A',
         replace:true,
@@ -11,6 +11,24 @@ angular.module('app').directive('appPositionList',[function () {
             data:'=' ,   //指令和控制器的$scope是同一个，降低指令和控制器的耦合度，有利于指令的复用
             filterObj:'=',
             name:'='
+        },
+        link:function ($scope) {
+            $scope.id = cache.get('id') || '';
+
+            $scope.select = function (obj) {
+                //点击的时候要发送请求，当后台数据发生改变之后，前端数据再改变
+                $http.post('data/favorite.json',{
+                    id:obj.id,
+                    select:!obj.select
+                }).success(function (res) {
+                    obj.select = !obj.select;
+                })
+
+
+
+
+            }
+
         }
     }
 }]);
